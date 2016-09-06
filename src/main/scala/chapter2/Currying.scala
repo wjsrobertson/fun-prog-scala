cd9ed02eb8d3ed3f11de806c9dadd1feee7e6214
wjsrobertson <Banana21!>
2016-09-06 22:01:02 +0100

    Added currying examples

---
+++ src/main/scala/chapter2/Currying.scala
@@ -0,0 +1,11 @@
+package chapter2
+
+object Currying {
+
+    def curry[A,B,C](f: (A, B) => C): A => (B => C) = a => b => f(a, b)
+
+    def uncurry[A,B,C](f: A => B => C): (A, B) => C = (a, b) => f(a)(b)
+
+    def compose[A,B,C](f: B => C, g: A => B): A => C = a => f(g(a))
+
+}
